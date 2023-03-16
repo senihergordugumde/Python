0e0614879dbf1f5349fbee75ce5aed6cf61c7176
BatuhanKurhan <113047782+BatuhanKurhan@users.noreply.github.com>
2023-03-16 18:48:27 +0300

    Add files via upload

---
+++ Sayı tahmin oyunu.py
@@ -0,0 +1,21 @@
+import random
+
+a = random.randint(1,100)
+hak = 5
+kere = 0
+while hak > 0:
+    hak -= 1
+    kere += 1
+    tahmin = int(input('Sayı tahmin et:'))
+
+    if tahmin == a :
+        print('Helal {} defada bildin. {} puan aldın.'.format(kere, (100 - (20 * kere))))
+        break
+    elif a > tahmin:
+        print('Çık')
+    else:
+        print('İn')
+    if hak == 0 :
+        print('Kaybettin. Sayı: {}'.format(a))
+    
+
