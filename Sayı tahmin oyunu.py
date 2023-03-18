76f553bf63237834e52859f800e676f98ed981bd
Emir <85408428+senihergordugumde@users.noreply.github.com>
2023-03-19 00:03:07 +0300

    Update Sayı tahmin oyunu.py

--- Sayı tahmin oyunu.py
+++ Sayı tahmin oyunu.py
@@ -3,19 +3,20 @@ import random
 a = random.randint(1,100)
 hak = 5
 kere = 0
-while hak > 0:
+
+while (hak > 0):
     hak -= 1
     kere += 1
     tahmin = int(input('Sayı tahmin et:'))
 
-    if tahmin == a :
+    if (tahmin == a) :
         print('Helal {} defada bildin. {} puan aldın.'.format(kere, (100 - (20 * kere))))
         break
-    elif a > tahmin:
+    elif (a > tahmin):
         print('Çık')
     else:
         print('İn')
-    if hak == 0 :
+    if (hak == 0) :
         print('Kaybettin. Sayı: {}'.format(a))
     
 
